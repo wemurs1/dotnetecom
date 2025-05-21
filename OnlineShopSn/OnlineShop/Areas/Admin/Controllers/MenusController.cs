6bafe614d2c301ac0ebe077e567b3b2a355c7455
Stuart Murray <stuart.murray.dev@outlook.com>
2025-05-21 12:48:13 +0100

    basic menu management

---
+++ OnlineShopSn/OnlineShop/Areas/Admin/Controllers/MenusController.cs
@@ -0,0 +1,143 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Areas.Admin
+{
+    [Area("Admin")]
+    public class MenusController(OnlineShopContext context) : Controller
+    {
+        // GET: Admin/Menus
+        public async Task<IActionResult> Index()
+        {
+            return View(await context.Menus.ToListAsync());
+        }
+
+        // GET: Admin/Menus/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await context.Menus
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return View(menu);
+        }
+
+        // GET: Admin/Menus/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Menus/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,MenuTitle,Link,Type")] Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(menu);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(menu);
+        }
+
+        // GET: Admin/Menus/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await context.Menus.FindAsync(id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+            return View(menu);
+        }
+
+        // POST: Admin/Menus/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,MenuTitle,Link,Type")] Menu menu)
+        {
+            if (id != menu.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    context.Update(menu);
+                    await context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MenuExists(menu.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(menu);
+        }
+
+        // GET: Admin/Menus/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await context.Menus
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return View(menu);
+        }
+
+        // POST: Admin/Menus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var menu = await context.Menus.FindAsync(id);
+            if (menu != null)
+            {
+                context.Menus.Remove(menu);
+            }
+
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MenuExists(int id)
+        {
+            return context.Menus.Any(e => e.Id == id);
+        }
+    }
+}
