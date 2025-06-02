4c4066b5cfd9f38480e516d70d73260d9e5ad6e4
Stuart Murray <stuart.murray.dev@outlook.com>
2025-06-02 13:27:08 +0100

    Simplify code, add async, corrected edit

--- OnlineShopSn/OnlineShop/Areas/Admin/Controllers/ProductsController.cs
+++ OnlineShopSn/OnlineShop/Areas/Admin/Controllers/ProductsController.cs
@@ -50,9 +50,9 @@ namespace OnlineShop.Areas.Admin.Controllers
                 if (MainImage != null)
                 {
                     product.ImageName = Guid.NewGuid().ToString() + System.IO.Path.GetExtension(MainImage.FileName);
-                    string ImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "products", product.ImageName);
+                    string ImagePath = GetFullFileName(product.ImageName);
                     using var stream = new FileStream(ImagePath, FileMode.Create);
-                    MainImage.CopyTo(stream);
+                    await MainImage.CopyToAsync(stream);
                 }
                 context.Add(product);
                 await context.SaveChangesAsync();
@@ -63,9 +63,9 @@ namespace OnlineShop.Areas.Admin.Controllers
                         var newGallery = new ProductGallery();
                         newGallery.ProductId = product.Id;
                         newGallery.ImageName = Guid.NewGuid().ToString() + System.IO.Path.GetExtension(item.FileName);
-                        string ImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "products", newGallery.ImageName);
+                        string ImagePath = GetFullFileName(newGallery.ImageName);
                         using var stream = new FileStream(ImagePath, FileMode.Create);
-                        item.CopyTo(stream);
+                        await item.CopyToAsync(stream);
                         context.ProductGalleries.Add(newGallery);
                     }
                     await context.SaveChangesAsync();
@@ -96,7 +96,12 @@ namespace OnlineShop.Areas.Admin.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,FullDesc,Price,Discount,ImageName,Qty,Tags,VideoUrl")] Product product)
+        public async Task<IActionResult> Edit(
+            int id,
+            [Bind("Id,Title,Description,FullDesc,Price,Discount,ImageName,Qty,Tags,VideoUrl")] Product product,
+            IFormFile? MainImage,
+            IFormFile[]? GalleryImages
+        )
         {
             if (id != product.Id)
             {
@@ -107,6 +112,25 @@ namespace OnlineShop.Areas.Admin.Controllers
             {
                 try
                 {
+                    if (MainImage != null)
+                    {
+                        string fn = GetFullFileName(product.ImageName!);
+                        if (System.IO.File.Exists(fn)) System.IO.File.Delete(fn);
+                        using var stream = new FileStream(fn, FileMode.Create);
+                        await MainImage.CopyToAsync(stream);
+                    }
+                    if (GalleryImages != null)
+                    {
+                        foreach (var item in GalleryImages)
+                        {
+                            var imageName = Guid.NewGuid() + Path.GetExtension(item.FileName);
+                            var fn = GetFullFileName(imageName);
+                            using var stream = new FileStream(fn, FileMode.Create);
+                            await item.CopyToAsync(stream);
+                            var galleryItem = new ProductGallery { ImageName = imageName, ProductId = product.Id };
+                            context.ProductGalleries.Add(galleryItem);
+                        }
+                    }
                     context.Update(product);
                     await context.SaveChangesAsync();
                 }
@@ -161,6 +185,24 @@ namespace OnlineShop.Areas.Admin.Controllers
         private bool ProductExists(int id)
         {
             return context.Products.Any(e => e.Id == id);
+        }
+
+        // GET: Admin/Products/DeleteGalley/5
+        public async Task<IActionResult> DeleteGallery(int id)
+        {
+            var gallery = await context.ProductGalleries.FirstOrDefaultAsync(x => x.Id == id);
+            if (gallery == null) return NotFound();
+
+            string fn = GetFullFileName(gallery.ImageName!);
+            if (System.IO.File.Exists(fn)) System.IO.File.Delete(fn);
+            context.ProductGalleries.Remove(gallery);
+            await context.SaveChangesAsync();
+            return Redirect($"edit/{gallery.ProductId}");
+        }
+
+        private string GetFullFileName(string imageName)
+        {
+            return Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "products", imageName);
         }
     }
 }
